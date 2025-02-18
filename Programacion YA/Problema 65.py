e5ba3ec6f2c4eb076b3c7b4b58d116a98bad563d
Manuel Gamez <138623553+Maanghel@users.noreply.github.com>
2025-02-18 13:47:12 -0700

    Agrega explicacion de funciones

--- Programacion YA/Problema 65.py
+++ Programacion YA/Problema 65.py
@@ -3,12 +3,14 @@ Confeccionar una función que reciba tres enteros y los muestre ordenados de men
 En otra función solicitar la carga de 3 enteros por teclado y proceder a llamar a la primer función definida.
 """
 
+# Funcion que ordena de menor a mayor
 def ordenar_menor_mayor(lista):
     lista_ordenada = sorted(lista)
     print("\nLista ordenada de menor a mayor:")
     for x in range(3):
         print(lista_ordenada[x])
 
+# Funcion que realiza la carga de 3 enteros
 def cargar_enteros():
     lista = []
     for x in range(3):
