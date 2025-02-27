2dea8818aad3d51a9d7103b70db2967c0ff01c62
Manuel Gamez <138623553+Maanghel@users.noreply.github.com>
2025-02-27 04:01:55 -0700

    Agrega las soluciones a los problemas 97 y 98

---
+++ Programacion YA/Problema 97.py
@@ -0,0 +1,29 @@
+"""
+Desarrollar una clase que represente un Cuadrado y tenga los siguientes métodos: 
+inicializar el valor del lado llegando como parámetro al método __init__ (definir un atributo llamado lado),
+imprimir su perímetro y su superficie.
+"""
+
+class Cuadrado:
+    def __init__(self, lado):
+        self.lado = lado
+    
+    def perimetro(self):
+        print(f"\nEl perimetro del cuadrado es de: {self.lado * 4}")
+    
+    def superficie(self):
+        print(f"El area del cuadrado es de: {self.lado * self.lado}")
+
+#########################   Main    ################################
+
+cuadrado1 = Cuadrado(2)
+cuadrado1.perimetro()
+cuadrado1.superficie()
+
+cuadrado2 = Cuadrado(4)
+cuadrado2.perimetro()
+cuadrado2.superficie()
+
+cuadrado3 = Cuadrado(6)
+cuadrado3.perimetro()
+cuadrado3.superficie()
---
+++ Programacion YA/Problema 98.py
@@ -0,0 +1,41 @@
+"""
+Implementar la clase Operaciones. Se deben cargar dos valores enteros por teclado en el método __init__, 
+calcular su suma, resta, multiplicación y división, cada una en un método, imprimir dichos resultados.
+"""
+
+class Operaciones:
+    def __init__(self):
+        self.valor1 = int(input("\nIngrese el primer valor: "))
+        self.valor2 = int(input("Ingrese el segundo valor: "))
+    
+    def suma(self):
+        print(f"La suma de {self.valor1} mas {self.valor2} es: {self.valor1 + self.valor2}")
+    
+    def resta(self):
+        print(f"La resta de {self.valor1} menos {self.valor2} es: {self.valor1 - self.valor2}")
+    
+    def multiplicación(self):
+        print(f"La multiplicacion de {self.valor1} por {self.valor2} es: {self.valor1 * self.valor2}")
+    
+    def división(self):
+        print(f"La division de {self.valor1} entre {self.valor2} es: {self.valor1 / self.valor2}")
+
+#########################   Main    ################################
+
+operacion1 = Operaciones()
+operacion1.suma()
+operacion1.resta()
+operacion1.multiplicación()
+operacion1.división()
+
+operacion2 = Operaciones()
+operacion2.suma()
+operacion2.resta()
+operacion2.multiplicación()
+operacion2.división()
+
+operacion3 = Operaciones()
+operacion3.suma()
+operacion3.resta()
+operacion3.multiplicación()
+operacion3.división()
