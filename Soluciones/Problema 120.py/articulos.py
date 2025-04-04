1289859fc58c92a12ef83b4063f3e5516a297160
Manuel Gamez <138623553+Maanghel@users.noreply.github.com>
2025-04-04 15:28:58 -0700

    Agrega la solucion al problema 120

---
+++ Soluciones/Problema 120.py/articulos.py
@@ -0,0 +1,53 @@
+import mysql.connector
+
+class Articulos:
+
+    def abrir(self):
+        conexion=mysql.connector.connect(host="localhost", 
+                                              user="root", 
+                                              passwd="", 
+                                              database="bd1")
+        return conexion
+
+
+    def alta(self, datos):
+        cone = self.abrir()
+        cursor = cone.cursor()
+        sql = "INSERT INTO articulos(descripcion, precio) VALUES (%s, %s)"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+
+    def consulta(self, datos):
+        cone = self.abrir()
+        cursor = cone.cursor()
+        sql = "SELECT descripcion, precio FROM articulos WHERE codigo= %s"
+        cursor.execute(sql, datos)
+        cone.close()
+        return cursor.fetchall()
+
+    def recuperar_todos(self):
+        cone = self.abrir()
+        cursor = cone.cursor()
+        sql = "SELECT codigo, descripcion, precio FROM articulos"
+        cursor.execute(sql)
+        cone.close()
+        return cursor.fetchall()
+
+    def baja(self, datos):
+        cone = self.abrir()
+        cursor = cone.cursor()
+        sql = "DELETE FROM articulos WHERE codigo= %s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas borradas
+
+    def modificacion(self, datos):
+        cone = self.abrir()
+        cursor = cone.cursor()
+        sql = "UPDATE articulos SET descripcion= %s, precio= %s where codigo= %s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas modificadas
